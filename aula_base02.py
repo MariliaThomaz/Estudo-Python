426251d8cd335ac4123bc54e03795e4691e400e4
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2024-02-27 22:45:54 -0300

    Assignment operators and repeat loop with continue

--- aula_base02.py
+++ aula_base02.py
@@ -26,7 +26,37 @@ print('Saio Loop')
 contador = 0
 
 while contador < 10:
-    contador = contador + 1
+    contador +=  1
+   # contador = contador + 1
+
     print(contador)
 
 print('Acabou')
+
+# opedores de atribuição
+#  += -= *= /= //= **= %=
+valor_cont = 4
+
+valor_cont += 4 
+print(f'+= {valor_cont}')
+valor_cont -= 4 
+print(f'-= {valor_cont}')
+valor_cont *= 4 
+print(f'*= {valor_cont}')
+valor_cont //= 4 
+print(f'//= {valor_cont}')
+valor_cont /= 4 
+print(f'/= {valor_cont}')
+valor_cont %= 4 
+print(f'%= {valor_cont}')
+
+cont = 0 
+while cont <= 30:
+    cont +=1
+
+    if cont == 27:
+        print("Não exiba 27")
+        continue # Ao colocar o continue ele pula o que faz e continua o loop
+    print(cont)
+    if cont == 29:
+        break
