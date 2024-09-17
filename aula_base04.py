94395f750a5f68b70a3f79297ac9ac0406af87a7
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2024-09-17 19:20:50 -0300

    Knowing a little about integrable and integrator.

---
+++ aula_base04.py
@@ -0,0 +1,30 @@
+#Explicando a importância de usar um laço de repetição while Em casos de repetições da 
+# qual você não sabe a quantidade de vezes que irá precisar executar alguma ação
+#  eis a necessidade de você usar esse laço de repetição "While"
+#Como esse exemplo de uma verificação de senha que está abaixo
+''' 
+senha_salva = '123456'
+senha_digitada = ''
+repeticoes =0
+
+while senha_salva != senha_digitada:
+    senha_digitada = input(f'Sua senha ({repeticoes}x): ')
+
+    repeticoes +=1
+
+print(repeticoes)
+print('Aquela ação acima pode ter repetições infinitas')
+'''
+# Usando o laço de repetição for
+
+texto = 'Eu amo voce'
+
+for letra in texto:
+    print(letra) #letra  É o inteirador 
+
+#For + Range
+#rage -> range(start, stop,  step)
+print('RANGE')
+numeros = range(0,100, 8) 
+for numero in numeros:
+    print(numero)
---
+++ aula_base05.py
@@ -0,0 +1,8 @@
+#Integrável --> str, sange , etc (__inter__)
+#Integrador quem sabe entregar em um valor por vez
+#next --> me ebtregue  o proximo  valor
+# inter --> entregue  seu iterador
+
+texto = iter('Marilia') #__iter__()
+
+print(texto.__next__())
