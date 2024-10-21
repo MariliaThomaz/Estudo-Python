6bda751547a70eb4a2fa80667714421465d025e5
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2024-10-21 20:32:57 -0300

    Commands in Python. Python best practice poem

---
+++ 000_comando.py
@@ -0,0 +1,38 @@
+#Comandos de Atalhos para Python
+
+#Comando para ver a versão do seu PY: python --version
+#Comando para ver a versão do seu PY: python -V
+#Comando para limpar a tela: cls
+#Comando para executar o módulo: python aula_base09.py (Infomar nome do aquivo)
+#Comando para não salvar nada no buffer: python -u 
+#Comando que executa uma biblioteca PY como um script: python -m mod
+#Comando Para criar ambiente virtual usando a biblioteca venv: python -m venv ambiente
+#comando que executa algo: python -c
+#Comando de interativo com Python: python 
+#Comando de interativo com Python Sair: quit() ou 
+#Comando entra  outro modulo:   python -i  (nome do aquivo)
+
+# The Zen of Python, por Tim Peters
+
+#Um poema dizendo o que não fazer e fazer
+'''
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor que complicado.
+Plano é melhor que aglomerado.
+Esparso é melhor que denso.
+Legibilidade conta.
+Casos especiais não são especiais o bastante para quebrar as regras.
+Embora a praticidade vença a pureza.
+Erros nunca devem passar silenciosamente.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, recuse a tentação de adivinhar.
+Deve haver um -- e só um -- modo óbvio para fazer algo.
+Embora esse modo possa não ser óbvio à primeira vista a menos que você seja holandês.
+Agora é melhor que nunca.
+Embora nunca frequentemente seja melhor que *exatamente* agora.
+Se a implementação é difícil de explicar, é uma má ideia.
+Se a implementação é fácil de explicar, pode ser uma boa ideia.
+Namespaces são uma grande ideia -- vamos fazer mais dessas!
+'''
