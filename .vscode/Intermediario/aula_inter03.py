5fc02851a417b3d80e6d9f36f3c69596cdef5a7e
MariliaThomaz <marilia.silva9@fatec.sp.gov.br>
2024-11-01 17:20:16 -0300

    Return of function values ​​(retunrn).

---
+++ .vscode/Intermediario/aula_inter03.py
@@ -0,0 +1,14 @@
+'''
+Retorno de valores das funçoes (retunrn)
+'''
+
+def soma(x,y):
+    #Pausar retorne você consegue obter o valor da 
+    # Operação ou ação desejada ele retorna apenas uma ação
+    return x + y
+
+somo1 = soma(2,2)
+soma2 = soma(3,3)
+
+#O printf é uma função, Mas ele só tem um Objetivo
+print(somo1+soma2)
